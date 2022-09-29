abad9f417d9df25ed18478d689c50ec7f01c0d77
braydenrobert <114121746+braydenrobert@users.noreply.github.com>
2022-09-28 23:28:21 -0400

    Update main.js

--- Assignment2/js/main.js
+++ Assignment2/js/main.js
@@ -1,18 +1,5 @@
 // WEB303 Assignment 2
 
-
-$("button").click(function(){
-
-$("#div1").load("", function(responseTxt, statusTxt, xhr){
-
-if(statusTxt == "success")
-
-	alert("External content loaded successfully!");
-
-if(statusTxt == "error")
-
-	alert("Error: " + xhr.status + ": " + xhr.statusText);
-
-});
-
+$("convert").click(function () {
+  $("#content").load("convert.html");
 });
