760a8b1c40c103ed67bca6d63897f6855b368d02
braydenrobert <114121746+braydenrobert@users.noreply.github.com>
2022-09-29 03:13:05 -0400

    Update main.js

--- Assignment2/js/main.js
+++ Assignment2/js/main.js
@@ -1,14 +1,17 @@
 // WEB303 Assignment 2
 
 $("#convert").click(function () {
+  $("#vretain").slideDown(1000);
   $("#convert").load("prospect.html");
 });
 
 $("#prospect").click(function () {
+  $("#vretain").slideDown(1000);
   $("#prospect").load("prospect.html");
 });
 
 $("#retain").click(function () {
-  $("#vretain").slideDown("slow");
+  $("#vretain").slideDown(1000);
   $("#retain").load("retain.html");
 });
+
