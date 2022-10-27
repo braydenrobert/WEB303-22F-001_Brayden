6b4b70138958915bb8cfee3d5a0d259c60e7a122
braydenrobert <114121746+braydenrobert@users.noreply.github.com>
2022-10-27 17:05:46 -0400

    Update script.js

--- Assignment_5/script.js
+++ Assignment_5/script.js
@@ -3,40 +3,39 @@
 */
 
 $(document).ready(function () {
-    const item = {
-        id: 1,
-        name: 'SUPER PEOPLE',
-        description:
-            'battle royale game developed by South Korean studio Wonder People, and published by Wonder Games.',
-        genre: 'Battle Royale',
-    };
+  const item = {
+    id: 1,
+    name: "SUPER PEOPLE",
+    description:
+      "battle royale game developed by South Korean studio Wonder People, and published by Wonder Games.",
+    genre: "Battle Royale"
+  };
 
-    const contentItem = new ContentItem(
-        item.id,
-        item.name,
-        item.description,
-        item.genre
-    );
+  const contentItem = new ContentItem(
+    item.id,
+    item.name,
+    item.description,
+    item.genre
+  );
 
-    $('#content-item-list').append(contentItem.toString());
-});
+  $("#content-item-list").append(contentItem.toString());
 
-class ContentItem {
+  class ContentItem {
     constructor(id, name, description, genre) {
-        this.id = id;
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.genre = genre;
+    }
+    updateContentItem(id, name, description, genre) {
+      if (this.id === id && name && description && genre) {
         this.name = name;
         this.description = description;
         this.genre = genre;
-    }
-    updateContentItem(id, name, description, genre) {
-        if (this.id === id && name && description && genre) {
-            this.name = name;
-            this.description = description;
-            this.genre = genre;
-        }
+      }
     }
     toString() {
-        return `
+      return `
     <div class="content-item-wrapper" id="content-item-${this.id}">
     <h2>${this.name}</h2>
     <p>${this.description}</p>
@@ -44,10 +43,19 @@ class ContentItem {
     </div>
     `;
     }
-	
-	const characters = ["Gatling Soldier", "Gas Soldier", "Nuclear", "Shotgun Master", "Strike Force"];
-	
-	$("content-item-wrapper").css('border', '1px solid black');
-	$("content-item-wrapper").css('border-width', '1px');
-	$("content-item-wrapper").css('padding', '25px 50px 75px 100px');
-}
+  }
+
+  const characters = [
+    "Gatling Soldier",
+    "Gas Soldier",
+    "Nuclear",
+    "Shotgun Master",
+    "Strike Force"
+  ];
+
+  $("#content-item-list").append(characters.toString());
+
+  $("content-item-wrapper").css("border", "1px solid black");
+  $("content-item-wrapper").css("border-width", "1px");
+  $("content-item-wrapper").css("padding", "25px 50px 75px 100px");
+});
