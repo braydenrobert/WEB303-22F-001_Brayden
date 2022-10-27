c37ea79a50855a21dbaa3cdbca1b34dce63638c2
braydenrobert <114121746+braydenrobert@users.noreply.github.com>
2022-10-27 17:13:29 -0400

    Update script.js

--- Assignment_5/script.js
+++ Assignment_5/script.js
@@ -20,31 +20,6 @@ $(document).ready(function () {
 
   $("#content-item-list").append(contentItem.toString());
 
-  class ContentItem {
-    constructor(id, name, description, genre) {
-      this.id = id;
-      this.name = name;
-      this.description = description;
-      this.genre = genre;
-    }
-    updateContentItem(id, name, description, genre) {
-      if (this.id === id && name && description && genre) {
-        this.name = name;
-        this.description = description;
-        this.genre = genre;
-      }
-    }
-    toString() {
-      return `
-    <div class="content-item-wrapper" id="content-item-${this.id}">
-    <h2>${this.name}</h2>
-    <p>${this.description}</p>
-    <div>${this.genre}</div>
-    </div>
-    `;
-    }
-  }
-
   const characters = [
     "Gatling Soldier",
     "Gas Soldier",
@@ -59,3 +34,28 @@ $(document).ready(function () {
   $("content-item-wrapper").css("border-width", "1px");
   $("content-item-wrapper").css("padding", "25px 50px 75px 100px");
 });
+
+class ContentItem {
+  constructor(id, name, description, genre) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.genre = genre;
+  }
+  updateContentItem(id, name, description, genre) {
+    if (this.id === id && name && description && genre) {
+      this.name = name;
+      this.description = description;
+      this.genre = genre;
+    }
+  }
+  toString() {
+    return `
+  <div class="content-item-wrapper" id="content-item-${this.id}">
+  <h2>${this.name}</h2>
+  <p>${this.description}</p>
+  <div>${this.genre}</div>
+  </div>
+  `;
+  }
+}
